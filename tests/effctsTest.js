34e843b8432384624899a7ffe9d70deddc63f963
Mikhail Cruz Andrade <jailandrade@gmail.com>
2015-02-28 00:50:44 -0600

    probando el primer test

---
+++ tests/effctsTest.js
@@ -0,0 +1,12 @@
+/**
+ * ECE.Effects
+ */
+
+// var expect = require("chai").expect;
+var should = require("chai").should()
+
+describe("Effects", function(){
+  describe("init", function(){
+      it('should return someValue when')
+  });
+});
